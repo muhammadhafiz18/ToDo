4928319fc88559d29f1785de017ef8726f4720e9
muhammadhafiz <khodjayev.muhammad@gmail.com>
2025-03-06 09:26:59 +0500

    added necessary configutaions in program cs

--- src/ToDo.Api/Program.cs
+++ src/ToDo.Api/Program.cs
@@ -1,4 +1,16 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ToDo.Api.Data;
+using ToDo.Api.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ToDoDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("Todo")));
+
+builder.Services.AddIdentity<User, IdentityRole<Guid>>()
+    .AddEntityFrameworkStores<ToDoDbContext>()
+    .AddDefaultTokenProviders();
 
 var app = builder.Build();
 
