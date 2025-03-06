935277c9df0b071c7461e6b315f2ddfb8ddd577c
muhammadhafiz <khodjayev.muhammad@gmail.com>
2025-03-06 10:51:52 +0500

    removed Identity provider

--- src/ToDo.Api/Program.cs
+++ src/ToDo.Api/Program.cs
@@ -1,16 +1,10 @@
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using ToDo.Api.Data;
-using ToDo.Api.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ToDoDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("Todo")));
-
-builder.Services.AddIdentity<User, IdentityRole<Guid>>()
-    .AddEntityFrameworkStores<ToDoDbContext>()
-    .AddDefaultTokenProviders();
 
 var app = builder.Build();
 
