1dca1855b1f0c61d6e80411b2c829316da8a914e
muhammadhafiz <khodjayev.muhammad@gmail.com>
2025-03-06 15:02:27 +0500

    added user auth controller

--- src/ToDo.Api/Program.cs
+++ src/ToDo.Api/Program.cs
@@ -5,6 +5,10 @@ using Microsoft.IdentityModel.Tokens;
 using ToDo.Api.Data;
 using ToDo.Api.Repository;
 using ToDo.Api.Services;
+using Microsoft.OpenApi.Models;
+using ToDo.Api.Validators;
+using FluentValidation;
+using ToDo.Api.Dtos;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -27,11 +31,56 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         };
     });
 
+builder.Services.AddAuthorization();
+
+builder.Services.AddControllers();
+
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<AuthService>();
+builder.Services.AddScoped<IValidator<RegisterRequest>, RegisterRequestValidator>();
+builder.Services.AddScoped<IValidator<LoginRequest>, LoginRequestValidator>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Todo API", Version = "v1" });
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme",
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
